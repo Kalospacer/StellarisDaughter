081a5f043afe47c7d8c5de38e1c16ff8507e3846
ProjectKoi-Kalo\Kalo <kalospacer@gmail.com>
2026-03-07 17:58:34 +0800

    zc

--- Source/StellarisDaughter/HarmonyPatches/NoBloodForWulaPatch.cs
+++ Source/StellarisDaughter/HarmonyPatches/NoBloodForWulaPatch.cs
@@ -3,12 +3,14 @@ using Verse;
 
 namespace StellarisDaughter
 {
-    [HarmonyPatch(typeof(Hediff_Injury), "get_BleedRate")]
+    // 改为 Patch 基类 Hediff，这样能同时覆盖 Hediff_Injury（伤口）和 Hediff_MissingPart（断肢）
+    [HarmonyPatch(typeof(Hediff), nameof(Hediff.BleedRate), MethodType.Getter)]
     public static class NoBloodForDaughter_BleedRate_Patch
     {
-        public static void Postfix(Hediff_Injury __instance, ref float __result)
+        public static void Postfix(Hediff __instance, ref float __result)
         {
-            if (__instance.pawn.def.defName == "SD_AI_Daughter_Race")
+            // 增加空值检查，防止红字报错
+            if (__instance?.pawn?.def?.defName == "SD_AI_Daughter_Race")
             {
                 __result = 0f;
             }
